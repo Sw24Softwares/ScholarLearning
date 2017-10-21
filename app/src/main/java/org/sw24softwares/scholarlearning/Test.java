23408821637a5a6e9e7a72469d09959e6a7c0761
Swarthon <swarthon.gokan@gmail.com>
2017-10-21 15:24:09 +0200

    Add verify button to TestActivity
    Convert LinearLayout to RelativeLayout which solves the rotating
    screen bug. Add comments.
    
    Solves : #1

--- app/src/main/java/org/sw24softwares/scholarlearning/Test.java
+++ app/src/main/java/org/sw24softwares/scholarlearning/Test.java
@@ -7,8 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.EditText;
+import android.widget.Button;
 
 import java.util.Random;
 import java.util.Vector;
@@ -16,7 +17,7 @@ import java.util.Vector;
 import static android.R.color.black;
 
 public class Test extends AppCompatActivity {
-        LinearLayout mLayout = null;
+        RelativeLayout mLayout = null;
         Format mFormat = null;
         Vector<EditText> mQuestions = new Vector<EditText>();
         
@@ -25,31 +26,54 @@ public class Test extends AppCompatActivity {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_test);
 
-                mLayout = (LinearLayout) findViewById (R.id.test_linearlayout);
-                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                                                                                 android.widget.LinearLayout.LayoutParams.MATCH_PARENT,
-                                                                                 android.widget.LinearLayout.LayoutParams.WRAP_CONTENT);                
-                
+                mLayout = (RelativeLayout) findViewById (R.id.test_linearlayout);
+
+                // Choose verb
                 Random rand = new Random();
                 int index = rand.nextInt(Loader.getSingleton().getNumDatas());
 
+                // Set edittext
                 mFormat = Loader.getSingleton().getData(index).getFormat();
                 for(int i = 0; i < mFormat.getCategories().size(); i++)
                         createQuestion(i);
 
+                // Set the given form
                 int given = rand.nextInt(Loader.getSingleton().getData(index).getInformations().size());
                 int givenQuestion = rand.nextInt(Loader.getSingleton().getData(index).getInformations().get(given).size());
                 mQuestions.get(given).setText(Loader.getSingleton().getData(index).getInformations().get(given).get(givenQuestion));
                 mQuestions.get(given).setEnabled(false);
                 mQuestions.get(given).setTextColor(getResources().getColor(black));
+
+                // Set the first edittext above the second
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                                                                                     android.widget.RelativeLayout.LayoutParams.MATCH_PARENT,
+                                                                                     android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT);
+                params.addRule(android.widget.RelativeLayout.ABOVE, mQuestions.get(1).getId());
+                mQuestions.get(0).setLayoutParams(params);
+
+                // Set verify button
+                params = new RelativeLayout.LayoutParams(
+                                                         android.widget.RelativeLayout.LayoutParams.MATCH_PARENT,
+                                                         android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT);
+                params.addRule(android.widget.RelativeLayout.BELOW, mQuestions.get(mQuestions.size()-1).getId());
+                ((Button)findViewById(R.id.test_verify)).setLayoutParams(params);
         }
         
         private void createQuestion(int index) {
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                                                                                     android.widget.RelativeLayout.LayoutParams.MATCH_PARENT,
+                                                                                     android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT);
+                
                 if(mFormat == null || mLayout == null)
                         return;
+
+                if(index > 0)
+                        params.addRule(android.widget.RelativeLayout.BELOW, mQuestions.get(index-1).getId());
+                
                 EditText question  = new EditText(this);
                 question.setHint(mFormat.getCategories().get(index));
-                //question.setLayoutParams(params);
+                question.setLayoutParams(params);
+                question.setId(index);
                 mLayout.addView(question);
                 mQuestions.addElement(question);
         }
